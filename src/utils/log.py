5e72b9da3b32a3c158a684d6b0b065753b7f6f1b
Eragonaz <muhammmadfikri6844@gmail.com>
2021-11-06 21:46:46 +0800

    Added more configs
    Can now be enable to disable specific streams of logging (file vs terminal). Configs is also provided in the constructor with a default config available for those not specificied.

--- src/utils/log.py
+++ src/utils/log.py
@@ -1,36 +1,48 @@
 import os, shutil, sys, logging
 from datetime import datetime 
 
+from utils.common import common
 
-formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
-log_config = {
-    "CLEAR_OUTPUT_ON_RESET" : True,
+FORMATTER = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+DEFAULT_CONFIG = {
+    "PRINT_TO_OUTPUT" : True,
+    "LOG_TO_FILE" : True,
+    
+    "CLEAR_OUTPUT_ON_RESET" : False,
+    
     "SHOW_STACK" : True,
-    "OUTPUT_LEVEL" : 1,
 }
 
 class Log:
 
-    def __init__(self, directory, name="cdc-helper", log_config=log_config):
+    def __init__(self, directory, name="cdc-helper", config=DEFAULT_CONFIG):
         log = logging.getLogger(name)
         log.setLevel(logging.INFO)
 
         self.logger = log  
         self.name = name
         self.directory = directory 
-        self.log_config = log_config  
-    
-        if log_config["CLEAR_OUTPUT_ON_RESET"]:
+        self.config = self._init_config(config)
+
+        if self.config["CLEAR_OUTPUT_ON_RESET"]:
             self.clean()
         
-        terminal_output = logging.StreamHandler(sys.stdout)
-        terminal_output.setFormatter(formatter)
-        log.addHandler(terminal_output)
+        if self.config["PRINT_TO_OUTPUT"]:
+            terminal_output = logging.StreamHandler(sys.stdout)
+            terminal_output.setFormatter(FORMATTER)
+            log.addHandler(terminal_output)
         
-        file_output = logging.FileHandler('{dir}/tracker_{date}.log'.format(dir=directory, date=datetime.today().strftime("%Y-%m-%d_%H-%M")))
-        file_output.setFormatter(formatter)
-        log.addHandler(file_output)
+        if self.config["LOG_TO_FILE"]:        
+            file_output = logging.FileHandler('{dir}/tracker_{date}.log'.format(dir=directory, date=datetime.today().strftime("%Y-%m-%d_%H-%M")))
+            file_output.setFormatter(FORMATTER)
+            log.addHandler(file_output)
         
+    def _init_config(self, config):
+        for configValue, configType in enumerate(DEFAULT_CONFIG):
+            if not common.checkKeyExistence(config, configType):
+                config[configType] = configValue
+        return config
+                    
     def clean(self, exceptFileName=""):
         for filename in os.listdir(self.directory):
             if filename != exceptFileName:
@@ -44,7 +56,7 @@ class Log:
                     self.error('Failed to delete %s. Reason: %s' % (file_path, e))   
         
     def info(self, msg):
-        if log_config["SHOW_STACK"]:
+        if self.config["SHOW_STACK"]:
             caller_info = self.logger.findCaller()
             
             self.logger.info("============================================")
