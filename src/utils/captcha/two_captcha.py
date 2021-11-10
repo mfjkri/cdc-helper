af28be19fc3e1c9c0ccad30224de90fe456c442a
Eragonaz <muhammadfikri6844@gmail.com>
2021-11-10 11:22:47 +0800

    Changed from internal debug_if to common->utils.debug_if
    Essentially moved the debug_if to utils.

--- src/utils/captcha/two_captcha.py
+++ src/utils/captcha/two_captcha.py
@@ -15,30 +15,30 @@ class Captcha:
         self.enabled = is_enabled
         self.debug_enabled = debug_enabled
     
-    def _debug_wrapper(self, debug_enabled, debug_type, message):
+    def _debug_wrapper(self, debug_enabled, debug_type, msg):
         if debug_enabled:
-            debug_type(message)
+            debug_type(msg)
         
     def solve(self, driver:webdriver, page_url:str=None):
         t_start = time.perf_counter()
         page_url = page_url or driver.current_url
         
         if self.enabled:
-            self._debug_wrapper(debug_enabled=self.debug_enabled, debug_type=self.log.debug, message="Solving CAPTCHA for: " + page_url)
+            self.log.debug_if(self.debug_enabled, f"Solving CAPTCHA for: {page_url}")
             site_key_element = driver.find_element_by_css_selector("[data-sitekey]")
             site_key = site_key_element.get_attribute("data-sitekey")
             
             try:
                 result = self.solver.recaptcha(sitekey=site_key, url=page_url)
-                self._debug_wrapper(debug_enabled=self.debug_enabled, debug_type=self.log.debug, message="Solved CAPTCHA!")
+                self.log.debug_if(self.debug_enabled, "Received 2Captcha response...")
             except TimeoutException as e:
-                self._debug_wrapper(debug_enabled=self.debug_enabled, debug_type=self.log.debug, message="2Captcha API has timed-out!")
+                self.log.debug_if(self.debug_enabled, f"2Captcha API has timed-out! : {str(e)}")
                 result = (False, f"TIMEOUT: {str(e)}")
             except NetworkException as e:
-                self._debug_wrapper(debug_enabled=self.debug_enabled, debug_type=self.log.debug, message="2Captcha API has encountered as a network error!")
+                self.log.debug_if(self.debug_enabled, f"2Captcha API has encountered a network error! : {str(e)}")
                 result = (False, f"NETWORK_ERROR: {str(e)}")
             except ApiException as e:
-                self._debug_wrapper(debug_enabled=self.debug_enabled, debug_type=self.log.debug, message="2Captcha API has encountered an API error!")
+                self.log.debug_if(self.debug_enabled, f"2Captha API has encountered an API error : {str(e)}")
                 result = (False, f"API_ERROR: {str(e)}")
             except Exception as e:
                 self.log.error(e)
@@ -46,15 +46,15 @@ class Captcha:
                 result = (False, f"UNKNOWN_ERROR : {str(e)}")
             else:
                 driver.execute_script("""document.querySelector('[name="g-recaptcha-response"]').innerText='{}'""".format(str(result["code"])))
-                self._debug_wrapper(debug_enabled=self.debug_enabled, debug_type=self.log.debug, message="Took {t} seconds to solve the reCaptcha using two-captcha!".format(t = time.perf_counter() - t_start))
+                self.log.debug_if(self.debug_enabled, "Took {t} seconds to solve the reCaptcha using two-captcha!".format(t = time.perf_counter() - t_start))
                 result = (True, f"SOLVED: {str(result)}")
             finally:
                 self.log.debug("t", result)
                 return result
         else:
-            self._debug_wrapper(debug_enabled=self.debug_enabled, debug_type=self.log.debug, message="Manually solving  CAPTCHA for: " + page_url)
+            self.log.debug_if(self.debug_enabled, f"Manually solving CAPTCHA for: {page_url}")
             input("Press enter to proceed: ")
-            self._debug_wrapper(debug_enabled=self.debug_enabled, debug_type=self.log.debug, message="Took exactly {t} seconds to solve the reCaptcha manually!".format(t = time.perf_counter() - t_start))
+            self.log.debug_if(self.debug_enabled, "Took exactly {t} seconds to solve the reCaptcha manually!".format(t = time.perf_counter() - t_start))
             return (True, "Manually solved CAPTCHA")
         
         
