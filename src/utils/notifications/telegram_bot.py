e2abd0bddf716f0255d9e4d71f26e0a790f219eb
Eragonaz <muhammadfikri6844@gmail.com>
2021-11-10 11:21:14 +0800

    Added RICH markup formatting for messages sent
    Messages sent will now be in the format <b>TITLE</b>\nMAILBODY

--- src/utils/notifications/telegram_bot.py
+++ src/utils/notifications/telegram_bot.py
@@ -8,6 +8,7 @@ class TelegramBot:
         self.default_chat_id = default_chat_id
         self.log = log
         
-    def send_message(self, text:str, chat_id:int=None):
-        chat_id = chat_id or self.default_chat_id
-        return requests.get(f"https://api.telegram.org/bot{self.token}/sendMessage?chat_id={chat_id}&text={text}")
+    def send_msg(self, msg_subject:str, msg_body:str, chat_id:int=None):
+        chat_id = str(chat_id or self.default_chat_id)
+        url = f"https://api.telegram.org/bot{self.token}/sendMessage?chat_id={chat_id}&text=<b>{msg_subject}</b>\n{msg_body}&parse_mode=HTML"
+        return requests.get(url)
