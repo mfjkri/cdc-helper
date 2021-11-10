0fa21806453099e27e8af65b9b80abbb3c66c5c4
Eragonaz <muhammadfikri6844@gmail.com>
2021-11-10 14:49:15 +0800

    Added a pretty print support to view CDC_Abstract attributes

--- abstracts/cdc_abstract.py
+++ abstracts/cdc_abstract.py
@@ -1,3 +1,5 @@
+import re
+
 class CDCAbstract:
     def __init__(self, username, password, headless=False):
         self.username = username
@@ -46,6 +48,61 @@ class CDCAbstract:
         self.lesson_name_pt = ''
         self.can_book_pt = True
 
+    def __str__(self):
+        #members = [attr for attr in dir(self) if not callable(getattr(self, attr)) and not attr.startswith("__")]
+        return re.sub(r'(^[ \t]+|[ \t]+(?=:))', '', 
+            f"""
+            # ------------------------------------- - ------------------------------------ #
+            CDC_ABSTRACT:
+            
+            user = {str(self.username)},
+            password = {str(self.password)},
+            headless = {str(self.headless)},
+
+            # Practical
+            available_days_practical = {str(self.available_days_practical)}
+            available_days_in_view_practical = {str(self.available_days_in_view_practical)}
+            available_times_practical = {str(self.available_times_practical)}
+            available_sessions_practical = {str(self.available_sessions_practical)}
+            booked_sessions_practical = {str(self.booked_sessions_practical)}
+            lesson_name_practical = {str(self.lesson_name_practical)}
+            can_book_next_practical_lesson = {str(self.can_book_next_practical_lesson)}
+            has_auto_reserved_practical = {str(self.has_auto_reserved_practical)}
+
+            # Road revision
+            available_days_rr = {str(self.available_days_rr)}
+            available_days_in_view_rr = {str(self.available_days_in_view_rr)}
+            available_times_rr = {str(self.available_times_rr)}
+            available_sessions_rr = {str(self.available_sessions_rr)}
+            booked_sessions_rr = {str(self.booked_sessions_rr)}
+
+            # BTT
+            available_days_btt = {str(self.available_days_btt)}
+            available_days_in_view_btt = {str(self.available_days_in_view_btt)}
+            available_times_btt = {str(self.available_times_btt)}
+            available_sessions_btt = {self.available_sessions_btt}
+            booked_sessions_btt = {str(self.booked_sessions_btt)}
+            lesson_name_btt = {str(self.lesson_name_btt)}
+
+            # RTT
+            available_days_rtt = {str(self.available_days_rtt)}
+            available_days_in_view_rtt = {str(self.available_days_in_view_rtt)}
+            available_times_rtt = {str(self.available_times_rtt)}
+            available_sessions_rtt = {str(self.available_sessions_rtt)}
+            booked_sessions_rtt = {str(self.booked_sessions_rtt)}
+            lesson_name_rtt = {str(self.lesson_name_rtt)}
+
+            # PT
+            available_days_pt = {str(self.available_days_pt)}
+            available_days_in_view_pt = {str(self.available_days_in_view_pt)}
+            available_times_pt = {str(self.available_times_pt)}
+            available_sessions_pt = {str(self.available_sessions_pt)}
+            booked_sessions_pt = {str(self.booked_sessions_pt)}
+            lesson_name_pt = {str(self.lesson_name_pt)}
+            can_book_pt = {str(self.can_book_pt)}
+            # ------------------------------------- - ------------------------------------ #
+            """, flags=re.M
+        )
 
 class Types:
     PRACTICAL = "practical"
