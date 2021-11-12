4f71a15afe049ed0248c2b21e42fc4206fde1dda
Eragonaz <muhammmadfikri6844@gmail.com>
2021-11-12 11:14:04 +0800

    Added monitoring for RTT and moved two_captcha configs all into one config dict

--- src/main.py
+++ src/main.py
@@ -15,8 +15,10 @@ if __name__ == "__main__":
     config = utils.load_config_from_yaml_file(file_path="config.yaml")
 
     log = Log(directory="logs/", name="cdc-helper", config=config["log_config"])
-    captcha_solver = TwoCaptcha(api_key=config["two_captcha_apikey"], log=log, is_enabled=True, debug_enabled=config["captcha_debug"])
+    captcha_solver = TwoCaptcha(log=log, config=config["two_captcha_config"])
     notification_manager = NotificationManager(log = log, mail_config=config["mail_config"], telegram_config=config["telegram_config"])
+
+    utils.clear_directory("temp/", log)
 
     with handler(login_credentials=config["cdc_login_credentials"], captcha_solver=captcha_solver, log=log, browser_type="firefox", headless=False) as cdc_handler:
         success_logging_in = cdc_handler.account_login()
@@ -27,7 +29,6 @@ if __name__ == "__main__":
             cdc_handler.open_booking_overview()
             cdc_handler.get_booked_lesson_date_time()
 
-            # Step 3: Check for practical lesson availability
             if monitored_types["practical"]:
                 if cdc_handler.open_practical_lessons_booking(field_type=Types.PRACTICAL):
                     if "REVISION" in cdc_handler.lesson_name_practical:
@@ -38,18 +39,25 @@ if __name__ == "__main__":
                         cdc_handler.get_all_available_sessions(field_type=Types.PRACTICAL)
                         if cdc_handler.can_book_next_practical_lesson:
                             log.info("Earlier date available!") 
-                            #inform_user_if_earlier_session_available(config["username"], field_type=Types.PRACTICAL)
+                            # TODO: Notify user if earlier sessions are available
                 else:
                     pass # No course found
             
-            # Step 4: Check for btt availability as well
             if monitored_types["btt"]:
                 if cdc_handler.open_theory_test_booking_page(field_type=Types.BTT):
                     cdc_handler.get_all_session_date_times(field_type=Types.BTT)
                     cdc_handler.get_all_available_sessions(field_type=Types.BTT)
-                    #inform_user_if_earlier_session_available(config['username'], field_type=Types.BTT)
+                    # TODO: Notify user if earlier sessions are available
                 else:
-                    log.debug("BTT not bookable for user")
+                    log.debug("User does not have BTT as an available option.")
+                    
+            if monitored_types["rtt"]:
+                if cdc_handler.open_theory_test_booking_page(field_type=Types.RTT):
+                    cdc_handler.get_all_session_date_times(field_type=Types.RTT)
+                    cdc_handler.get_all_available_sessions(field_type=Types.RTT)
+                    # TODO: Notify user if earlier sessions are available
+                else:
+                    log.debug("User does not have RTT as an available option.")
                     
             # TODO: Add more checks (RTT, PT)
 
