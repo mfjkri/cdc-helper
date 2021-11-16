de00194837d1ee0983ee2c11bde4a9c5363076e0
Eragonaz <muhammadfikri6844@gmail.com>
2021-11-16 09:50:29 +0800

    Modified abstract to be dynamic in nature
    Instead of manually setting the layout and attributes in the layout, using a template we can create and set these attributes for every field_type dynamically at runtime.

--- abstracts/cdc_abstract.py
+++ abstracts/cdc_abstract.py
@@ -1,145 +1,75 @@
 import re
 from typing import Any
 
+attribute_templates = [
+    ["available_days", list],
+    ["available_days_in_view", list],
+    ["available_times", list],
+    ["available_sessions", dict],
+    ["earlier_available_sessions", dict],
+    ["booked_sessions", dict],
+    ["lesson_name", str]
+]
+
+class Types:
+    SIMULATOR = "simulator"
+    PRACTICAL = "practical"
+    ROAD_REVISION = "rr"
+    ETT = "ett"
+    BTT = "btt"
+    RTT = "rtt"
+    PT = "pt"
+
 class CDCAbstract:
     def __init__(self, username, password, headless=False):
         self.username = username
         self.password = password
         self.headless = headless
         
+        field_types = [attr for attr in dir(Types) if not callable(getattr(Types, attr)) and not attr.startswith("__")]
+        for _, field_type in enumerate(field_types):
+            field_type_str = getattr(Types, field_type)
+            
+            for i in range(0, len(attribute_templates)):
+                attribute_template = attribute_templates[i]
+                setattr(self, f"{attribute_template[0]}_{field_type_str}", attribute_template[1]())
+                
          # Simulator
-        self.available_days_simulator = []
-        self.available_days_in_view_simulator = []
-        self.available_times_simulator = []
-        self.available_sessions_simulator = {}
-        self.booked_sessions_simulator = {}
-        self.lesson_name_simulator = ''
         self.can_book_next_simulator = True
         self.has_auto_reserved_simulator = False
 
         # Practical
-        self.available_days_practical = []
-        self.available_days_in_view_practical = []
-        self.available_times_practical = []
-        self.available_sessions_practical = {}
-        self.booked_sessions_practical = {}
-        self.lesson_name_practical = ''
         self.can_book_next_practical_lesson = True
         self.has_auto_reserved_practical = False
 
-        # Road revision
-        self.available_days_rr = []
-        self.available_days_in_view_rr = []
-        self.available_times_rr = []
-        self.available_sessions_rr = {}
-        self.booked_sessions_rr = {}
-        
-        # E-Trial Theory Test
-        self.available_days_ett = []
-        self.available_days_in_view_ett = []
-        self.available_times_ett = []
-        self.available_sessions_ett = {}
-        self.booked_sessions_ett = {}
-        self.lesson_name_ett = ''
-
-        # BTT
-        self.available_days_btt = []
-        self.available_days_in_view_btt = []
-        self.available_times_btt = []
-        self.available_sessions_btt = {}
-        self.booked_sessions_btt = {}
-        self.lesson_name_btt = ''
-
-        # RTT
-        self.available_days_rtt = []
-        self.available_days_in_view_rtt = []
-        self.available_times_rtt = []
-        self.available_sessions_rtt = {}
-        self.booked_sessions_rtt = {}
-        self.lesson_name_rtt = ''
-
         # PT
-        self.available_days_pt = []
-        self.available_days_in_view_pt = []
-        self.available_times_pt = []
-        self.available_sessions_pt = {}
-        self.booked_sessions_pt = {}
-        self.lesson_name_pt = ''
         self.can_book_pt = True
+        
 
     def __str__(self):
         #members = [attr for attr in dir(self) if not callable(getattr(self, attr)) and not attr.startswith("__")]
-        return re.sub(r'(^[ \t]+|[ \t]+(?=:))', '', 
-            f"""
-            # ------------------------------------- - ------------------------------------ #
-            CDC_ABSTRACT:
-            
-            user = {str(self.username)},
-            password = {str(self.password)},
-            headless = {str(self.headless)},
-            
-            # Simulator
-            available_days_simulator = {str(self.available_days_simulator)}
-            available_days_in_view_simulator = {str(self.available_days_in_view_simulator)}
-            available_times_simulator = {str(self.available_times_simulator)}
-            available_sessions_simulator = {str(self.available_sessions_simulator)}
-            booked_sessions_simulator = {str(self.booked_sessions_simulator)}
-            lesson_name_simulator = {str(self.lesson_name_practical)}
-            can_book_next_simulator = {str(self.can_book_next_simulator)}
-            has_auto_reserved_simulator = {str(self.has_auto_reserved_simulator)}
+        blacklist_attr_names = "captcha_solver,"
+        abstract_str = "# ------------------------------------- - ------------------------------------ #\n"
+        abstract_str += "CDC_ABSTRACT\n"
+        
+        abstract_str += f"user = {str(self.username)}"
+        abstract_str += f"password = {str(self.password)}"
+        abstract_str += f"headless = {str(self.headless)}"
 
-            # Practical
-            available_days_practical = {str(self.available_days_practical)}
-            available_days_in_view_practical = {str(self.available_days_in_view_practical)}
-            available_times_practical = {str(self.available_times_practical)}
-            available_sessions_practical = {str(self.available_sessions_practical)}
-            booked_sessions_practical = {str(self.booked_sessions_practical)}
-            lesson_name_practical = {str(self.lesson_name_practical)}
-            can_book_next_practical_lesson = {str(self.can_book_next_practical_lesson)}
-            has_auto_reserved_practical = {str(self.has_auto_reserved_practical)}
+        abstract_str += "\n"
+        
+        field_types = [attr for attr in dir(Types) if not callable(getattr(Types, attr)) and not attr.startswith("__")]
+        abstract_attr = [attr for attr in dir(self) if not callable(getattr(self, attr)) and not attr.startswith("__")]
+        for _, field_type in enumerate(field_types):
+            abstract_str += f"# {str(field_type)}\n"
 
-            # Road revision
-            available_days_rr = {str(self.available_days_rr)}
-            available_days_in_view_rr = {str(self.available_days_in_view_rr)}
-            available_times_rr = {str(self.available_times_rr)}
-            available_sessions_rr = {str(self.available_sessions_rr)}
-            booked_sessions_rr = {str(self.booked_sessions_rr)}
-            
-            # ETT
-            available_days_ett = {str(self.available_days_ett)}
-            available_days_in_view_ett = {str(self.available_days_in_view_ett)}
-            available_times_ett = {str(self.available_times_ett)}
-            available_sessions_ett = {self.available_sessions_ett}
-            booked_sessions_ett = {str(self.booked_sessions_ett)}
-            lesson_name_ett = {str(self.lesson_name_ett)}
-
-            # BTT
-            available_days_btt = {str(self.available_days_btt)}
-            available_days_in_view_btt = {str(self.available_days_in_view_btt)}
-            available_times_btt = {str(self.available_times_btt)}
-            available_sessions_btt = {self.available_sessions_btt}
-            booked_sessions_btt = {str(self.booked_sessions_btt)}
-            lesson_name_btt = {str(self.lesson_name_btt)}
-
-            # RTT
-            available_days_rtt = {str(self.available_days_rtt)}
-            available_days_in_view_rtt = {str(self.available_days_in_view_rtt)}
-            available_times_rtt = {str(self.available_times_rtt)}
-            available_sessions_rtt = {str(self.available_sessions_rtt)}
-            booked_sessions_rtt = {str(self.booked_sessions_rtt)}
-            lesson_name_rtt = {str(self.lesson_name_rtt)}
-
-            # PT
-            available_days_pt = {str(self.available_days_pt)}
-            available_days_in_view_pt = {str(self.available_days_in_view_pt)}
-            available_times_pt = {str(self.available_times_pt)}
-            available_sessions_pt = {str(self.available_sessions_pt)}
-            booked_sessions_pt = {str(self.booked_sessions_pt)}
-            lesson_name_pt = {str(self.lesson_name_pt)}
-            can_book_pt = {str(self.can_book_pt)}
-            # ------------------------------------- - ------------------------------------ #
-            """, flags=re.M
-        )
+            field_type_str = getattr(Types, field_type)
+            for _, attr in enumerate(abstract_attr):
+                if (field_type_str in attr) and (not attr in blacklist_attr_names):
+                    abstract_str += f"# {str(attr)} = {str(getattr(self, attr))}\n"
+            abstract_str += "\n"
+        abstract_str += "# ------------------------------------- - ------------------------------------ #"
+        return abstract_str
         
     def get_attribute(self, attribute:str):
         return getattr(self, attribute)
@@ -153,11 +83,3 @@ class CDCAbstract:
     def set_attribute_with_fieldtype(self, attribute:str, field_type:str, value:Any):
         setattr(self, f"{attribute}_{field_type}", value)
 
-class Types:
-    SIMULATOR = "simulator"
-    PRACTICAL = "practical"
-    ROAD_REVISION = "rr"
-    ETT = "ett"
-    BTT = "btt"
-    RTT = "rtt"
-    PT = "pt"
